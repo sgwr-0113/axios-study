dc067024d5bce4c91109bfe4bd51a37d7e0b54d9
sgwr-0113 <xxepisode.18xx@gmail.com>
2022-02-11 11:10:41 +0900

    fix path

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -2,14 +2,14 @@ import baseAxios from './axios';
 
 class PostApi {
   async getPost<T>(params: { [key: string]: string | number | unknown }, postId?: string | number): Promise<T> {
-    const res = await baseAxios.get<T>(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+    const res = await baseAxios.get<T>(`/posts/${postId}`, {
       params,
     });
     return res.data;
   }
 
   async getPosts<T>(): Promise<T[]> {
-    const res = await baseAxios.get<T[]>('https://jsonplaceholder.typicode.com/posts');
+    const res = await baseAxios.get<T[]>('/posts');
     return res.data;
   }
 }
